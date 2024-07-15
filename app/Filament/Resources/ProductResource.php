8397c0a21114d6d3753764bf48dc19ccb1c82b6d
YeaminRabbi <yeaminrabbi308@gmail.com>
2024-07-15 14:29:57 +0600

    feat: filament file upload

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -14,6 +14,11 @@ use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Filament\Forms\Components\BelongsToSelect;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Columns;
+
 
 class ProductResource extends Resource
 {
@@ -24,14 +29,22 @@ class ProductResource extends Resource
     public static function form(Form $form): Form
     {
         return $form
-        ->schema([
-            Forms\Components\TextInput::make('name')->required(),
-            Forms\Components\TextInput::make('code')->required(),
-            Forms\Components\TextInput::make('details')->required(),
-            BelongsToSelect::make('category_id')
-            ->relationship('category', 'name')
-            ->required(),
-        ]);
+            ->schema([
+                Forms\Components\TextInput::make('name')->required(),
+                Forms\Components\TextInput::make('code')->required(),
+                BelongsToSelect::make('category_id')
+                ->relationship('category', 'name')
+                ->required(),
+                FileUpload::make('image')
+                    ->disk('local')
+                    ->directory('products') 
+                    ->image()
+                    ->imageEditor(),
+                RichEditor::make('details')->required(),
+                
+            ]);
+
+        
     }
 
     public static function table(Table $table): Table
@@ -41,12 +54,19 @@ class ProductResource extends Resource
                 Tables\Columns\TextColumn::make('name')->label('Product Name'),
                 Tables\Columns\TextColumn::make('code')->label('Product Code'),
                 Tables\Columns\TextColumn::make('category.name')->label('Product Category'),
+                Tables\Columns\ImageColumn::make('image')->label('Product Image')
+                ->getStateUsing(function (Product $record): string {
+                    return asset('images/' . $record->image); // Ensure the correct URL
+                })
+                ->circular()
+                ->size(50)
             ])
             ->filters([
-                
+                //
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
